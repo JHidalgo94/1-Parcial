8ea5bf3c5119641fa032679163bfbd183c15f341
JHidalgo94 <124218640+JHidalgo94@users.noreply.github.com>
2023-02-03 15:46:44 -0600

    Update Form1.vb

--- Tarea 1 LP3 Secion 2000 Jorge Hidalgo/Tarea 1 LP3 Secion 2000 Jorge Hidalgo/Form1.vb
+++ Tarea 1 LP3 Secion 2000 Jorge Hidalgo/Tarea 1 LP3 Secion 2000 Jorge Hidalgo/Form1.vb
@@ -1,7 +1,17 @@
 ﻿Public Class Form1
     Private Sub CalcularButton1_Click(sender As Object, e As EventArgs) Handles CalcularButton1.Click
-        Decimal numero = Convert.ToDecimal CalcularTextBox1.Text;
+        Dim numero As Decimal = Convert.ToDecimal(CalcularTextBox1.Text)
 
+        If numero Mod 2 = 0 Then
+            MsgBox("El numero es par")
+        ElseIf numero Mod 2 <> 0 Then
+            MsgBox("El numero es Impar")
+        End If
 
+        If numero > 0 Then
+            MsgBox("El numero es positivo")
+        ElseIf numero < 0 Then
+            MsgBox("El numero es negativo")
+        End If
     End Sub
 End Class
